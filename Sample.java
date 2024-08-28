2acc17b2a4cfbf90908df97a8b051dbc65c4b405
reddybr <tanuboddib@gmail.com>
2024-08-27 20:53:06 -0400

    Sample Commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+package coding;
+import java.util.Scanner;
+
+class Sample{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("Num: " + n);
+    }
+}
